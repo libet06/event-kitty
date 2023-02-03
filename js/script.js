0fddc977f238ab3aef0a71f3380d30fcb3ff0a0f
Libet <119543233+libet06@users.noreply.github.com>
2023-02-03 10:49:18 -0500

    Added and removed classes

--- js/script.js
+++ js/script.js
@@ -6,5 +6,17 @@ var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function() {
    // console.log("It's working.");
-   cat.classList.add("show");
+   //cat.classList.add("show");
+   if (cat.classList.contains("show")) {
+    //console.log("yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+   } else {
+    //console.log("no");
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+   }
+   
 });
